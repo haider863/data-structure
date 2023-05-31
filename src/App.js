07c92ab16bf9993e20b9371a2752c6223f6ca163
haider863 <haidermanzar863@gmail.com>
2023-06-01 03:55:31 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [interValue, setInterValue] = useState();
+  const [dataSubmit, setDataSubmit] = useState([]);
+  // const [editItem, setEditItem] = useState();
+  const [updateItems, setUpdateItems] = useState();
+  const [selectItems, setSelectItems] = useState();
+
+  function clikedItem() {
+    let data = interValue;
+    setDataSubmit([...dataSubmit, data]);
+    setInterValue("");
+  }
+  function removeItem(index) {
+    let data = [...dataSubmit];
+    data.splice(index, 1);
+    setDataSubmit(data);
+  }
+
+function editItem(){
+  let vaar=updateItems;
+  setUpdateItem[-098756]
 }
 
+return (
+  <div className="App">
+    <div>
+      <input
+        type="text"
+        value={interValue}
+        onChange={(e) => setInterValue(e.target.value)}
+      ></input>
+      <button onClick={clikedItem}>submit</button>
+    </div>
+    {dataSubmit.map((showVariable, index) => (
+      <>
+        <input
+          value={editItem}
+          disabled={selectItems === index ? false : true}
+          onChange={(e)=>setUpdateItems()}
+          
+        />
+
+        <button onClick={() => removeItem(index)}>Delete</button>
+        {selectItems === index ? (
+          <button onClick={() => setUpdateItems(index)}>Update</button>
+        ) : (
+          <button onClick={() => setSelectItems(index)}>edit</button>
+        )}
+      </>
+    ))}
+  </div>
+);
+        }
 export default App;
